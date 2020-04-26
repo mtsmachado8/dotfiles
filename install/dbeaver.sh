995cc2ef1864687e72c044ceaa57aeda6ff28eec
Mateus Machado <mtsmachado8@gmail.com>
2020-04-26 13:18:49 -0300

    feat: dbeaver and skype

---
+++ install/dbeaver.sh
@@ -0,0 +1,5 @@
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | apt-key add -
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | tee /etc/apt/sources.list.d/dbeaver.list
+apt-get update -y
+apt-get install dbeaver-ce -y
+apt policy  dbeaver-ce 
